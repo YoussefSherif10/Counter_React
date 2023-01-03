8b7b6146705ef8e3d4a2c29892edf755008a6be9
yousseffekry <mr-youssefsherif@hotmail.com>
2023-01-03 12:56:51 +0200

    Moved the state and event handlers from Counters Component to App component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,64 @@ import logo from './logo.svg';
 import './App.css';
 import Counters from "./components/counters";
 import Navbar from "./components/navbar";
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar />
-        <Counters />
-    </div>
-  );
+class App extends Component{
+    state = {
+        /* the component that contains the state should be the one to modify it. that's why we raise an event
+         from Counter and handle the event here [handleDelete].
+         implementation: we create handleDelete method and pass its reference as a prop. then in the child component
+         we call it by reference.
+         */
+        counters: [
+            {id: 1 , value: 0},
+            {id: 2 , value: 0},
+            {id: 3 , value: 0},
+            {id: 4 , value: 0},
+        ],
+    };
+
+  render() {
+      return (
+          <div className="App">
+              <Navbar />
+              <Counters
+                  onDelete={this.handleDelete}
+                  onIncrement={this.handleIncrement}
+                  onReset={this.handleReset}
+                  counters={this.state.counters}
+              />
+          </div>
+      );
+  }
+
+    handleDelete = (counterId) => {
+        /*const item = this.state.counters.find(counter => counter.id === counterId );
+        const index = this.state.counters.indexOf(item);
+        this.setState({counter: this.state.counters.splice(index, 1)});
+         */
+
+        const counters = this.state.counters.filter(counter => counter.id !== counterId);
+        this.setState({ counters });
+    }
+
+    handleIncrement = (counter) => {
+        /* this function handles the increment of the counter. this gets passed by reference
+        * so I am making it arrow function to set this value to the class */
+        // override the state with the new value
+
+        const index = this.state.counters.indexOf(counter);
+        counter.value++;
+        const counters = [...this.state.counters];
+        counters[index] = counter;
+        this.setState({counters});
+
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(counter => {counter.value = 0 ; return counter});
+        this.setState({counters});
+    }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,65 +2,23 @@ import React, {Component} from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-    state = {
-        /* the component that contains the state should be the one to modify it. that's why we raise an event
-         from Counter and handle the event here [handleDelete].
-         implementation: we create handleDelete method and pass its reference as a prop. then in the child component
-         we call it by reference.
-         */
-        counters: [
-            {id: 1 , value: 0},
-            {id: 2 , value: 0},
-            {id: 3 , value: 0},
-            {id: 4 , value: 0},
-        ],
-    };
-
     render() {
         return (
             <main>
             <div className="col-lg-8 mx-auto p-4 py-md-5">
-                <button onClick={this.handleReset} className="btn btn-primary btn-lg">Reset</button>
-                {this.state.counters.map(counter =>
+                <button onClick={this.props.onReset} className="btn btn-primary btn-lg">Reset</button>
+                {this.props.counters.map(counter =>
                     <Counter
                         key={counter.id}
-                        onDelete={this.handleDelete}
+                        onDelete={this.props.onDelete}
                         counter={counter}
-                        onIncrement={this.handleIncrement}
+                        onIncrement={this.props.onIncrement}
                     />)}
             </div>
             </main>
         );
     }
 
-    //Event handler
-    handleDelete = (counterId) => {
-        /*const item = this.state.counters.find(counter => counter.id === counterId );
-        const index = this.state.counters.indexOf(item);
-        this.setState({counter: this.state.counters.splice(index, 1)});
-         */
-
-        const counters = this.state.counters.filter(counter => counter.id !== counterId);
-        this.setState({ counters });
-    }
-
-    handleIncrement = (counter) => {
-        /* this function handles the increment of the counter. this gets passed by reference
-        * so I am making it arrow function to set this value to the class */
-        // override the state with the new value
-
-        const index = this.state.counters.indexOf(counter);
-        counter.value++;
-        const counters = [...this.state.counters];
-        counters[index] = counter;
-        this.setState({counters});
-
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(counter => {counter.value = 0 ; return counter});
-        this.setState({counters});
-    }
 }
 
 export default Counters;
